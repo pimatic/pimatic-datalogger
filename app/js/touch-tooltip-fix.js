e8f17b6a2d013fc73ff3cef785f884b3541d5370
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2014-01-24 20:48:24 +0100

    finished touch scroll over highchart fix.

--- app/js/touch-tooltip-fix.js
+++ app/js/touch-tooltip-fix.js
@@ -1,45 +1,65 @@
 Highcharts.Chart.prototype.callbacks.push(function(chart) {
   var hasTouch = document.documentElement.ontouchstart !== undefined,
-      mouseTracker = chart.pointer,
-      container = chart.container,
-      mouseMove;
- 
-  mouseMove = function (e) {
-    if (hasTouch) {
+      container = chart.container;
+
+  callTrough = function(f) {
+    return function(e) {
+      // if we have touch
+      if (hasTouch) {
+        // and it is a multitouch event
         if (e && e.touches && e.touches.length > 1) {
-            mouseTracker.onContainerTouchMove(e);
+          return f(e);
         } else {
-            // normalize
-            e = this.normalize(e);    
-            if (this.inClass(e.target, 'highcharts-tracker') || 
-              chart.isInsidePlot(e.chartX - chart.plotLeft, e.chartY - chart.plotTop)) {
-              console.log('yes');
-              mouseTracker.onContainerMouseMove(e);
-            } else {
-              console.log('no');
-            }
+          if (chart.pointer.inClass(e.target, 'highcharts-tracker') || 
+            chart.isInsidePlot(e.chartX - chart.plotLeft, e.chartY - chart.plotTop)) {
+            //let system handle the event ot allow scralling
+            setTimeout(function() {
+              f(e);
+            }, 1);
             return;
+          } else {
+            // call through
+            return f(e);
+          }
         }
-    } else {
-                  // normalize
-            e = this.normalize(e);    
-            if (this.inClass(e.target, 'highcharts-tracker') || 
-              chart.isInsidePlot(e.chartX - chart.plotLeft, e.chartY - chart.plotTop)) {
-              console.log('yes');
-              mouseTracker.onContainerMouseMove(e);
-            } else {
-              console.log('no');
-            }
+      } else {
+        //no touch so call through
+        return f(e);
+      }
     }
   };
-  
-  click = function (e) {
-    if (hasTouch) { 
-        mouseTracker.onContainerMouseMove(e);      
-    }
-    mouseTracker.onContainerClick(e);    
-  }
+
+  container.ontouchstart = callTrough(container.ontouchstart, 'touchstart');
+  container.onmousemove = callTrough(container.ontouchmove, 'touchmove');
+  container.ontouchmove = callTrough(container.onmousemove, 'mousemove');
  
-  container.onmousemove = container.ontouchstart = container.ontouchmove = mouseMove;
-  container.onclick = click;
+  // mouseMove = function (e) {
+  //   if (hasTouch) {
+  //     if (e && e.touches && e.touches.length > 1) {
+  //       mouseTracker.onContainerTouchMove(e);
+  //     } else {
+  //       if (mouseTracker.inClass(e.target, 'highcharts-tracker') || 
+  //         chart.isInsidePlot(e.chartX - chart.plotLeft, e.chartY - chart.plotTop)) {
+  //         ;
+  //       } else {
+  //         mouseTracker.onContainerTouchMove(e);
+  //       }
+  //       return
+  //     }
+  //   } else {
+  //     mouseTracker.onContainerMouseMove(e);
+  //   }
+  // };
+  
+  // click = function (e) {
+  //   if (hasTouch) {
+  //       mouseTracker.onContainerMouseMove(e);   
+  //   }
+  //   mouseTracker.onContainerClick(e);
+  // };
+ 
+  // container.onmousemove = container.ontouchstart = container.ontouchmove = mouseMove;
+  // container.onclick = click;
+
+  console.log( container.onmousemove);
 });
